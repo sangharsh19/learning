efe1ffe50d68919c9502f761b14624e290510e4c
sangharsh19 <95059216+sangharsh19@users.noreply.github.com>
2022-01-01 18:15:26 +0530

    Add files via upload

--- temp.cpp
+++ temp.cpp
@@ -1,17 +1,24 @@
-#include<iostream>
+#include <iostream>
+#include<list>
+#include<iterator>
 using namespace std;
 
+   
+int expof2(int n)
+{
+    if(n == 0)
+    {
+        return 1;
+    }
+    return 2*expof2(n-1);
+
+}
 int main()
 {
-    float f,cel;
-    //farhenit to celsius
-    cout<<"enter temp in farhenit"<<endl;
-    cin>>f;
-    cel = ((f*5)-(5*32))/9;
-    cout<<"temp in celsius is: "<<cel<<endl;
-
-
-
+    cout<<" enter term "<<endl;
+    int n;
+    cin>>n;
+    cout<<endl<<expof2(n)<<endl;
 
     return 0;
 }
